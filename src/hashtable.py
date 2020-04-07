b12472064be30deca45b39b9eaf0be26a8d6d418
Mark Halls <mark.halls@gmail.com>
2020-04-06 17:32:07 -0700

    Create hash table, don't handle collisions can add, remove and resize

--- src/hashtable.py
+++ src/hashtable.py
@@ -7,88 +7,106 @@ class LinkedPair:
         self.value = value
         self.next = None
 
+    def find(self, key):
+        if self.key is key:
+            return self.value
+        if self.next is None:
+            return None
+
+        return self.next.find(key)
+
+
 class HashTable:
-    '''
+    """
     A hash table that with `capacity` buckets
     that accepts string keys
-    '''
+    """
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
 
-
     def _hash(self, key):
-        '''
+        """
         Hash an arbitrary key and return an integer.
 
         You may replace the Python hash with DJB2 as a stretch goal.
-        '''
+        """
         return hash(key)
 
-
     def _hash_djb2(self, key):
-        '''
+        """
         Hash an arbitrary key using DJB2 hash
 
         OPTIONAL STRETCH: Research and implement DJB2
-        '''
+        """
         pass
 
-
     def _hash_mod(self, key):
-        '''
+        """
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
-        '''
+        """
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
-        '''
+        """
         Store the value with the given key.
 
         # Part 1: Hash collisions should be handled with an error warning. (Think about and
         # investigate the impact this will have on the tests)
-
+        """
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            return print(f"Error: {key} already exists")
+        self.storage[index] = LinkedPair(key, value)
+        """
         # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
 
         Fill this in.
-        '''
-        pass
-
-
+        """
 
     def remove(self, key):
-        '''
+        """
         Remove the value stored with the given key.
 
         Print a warning if the key is not found.
 
         Fill this in.
-        '''
-        pass
+        """
+        index = self._hash_mod(key)
 
+        if self.storage[index] is None:
+            return print(f"Warning: key {key} not found")
+
+        self.storage[index] = None
 
     def retrieve(self, key):
-        '''
+        """
         Retrieve the value stored with the given key.
 
         Returns None if the key is not found.
 
         Fill this in.
-        '''
-        pass
+        """
+        if self.storage[self._hash_mod(key)] is None:
+            return None
 
+        return self.storage[self._hash_mod(key)].find(key)
 
     def resize(self):
-        '''
+        """
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
 
         Fill this in.
-        '''
-        pass
-
+        """
+        self.capacity *= 2
+        temp = [None] * self.capacity
+        for item in self.storage:
+            if item is not None:
+                temp[self._hash_mod(item.key)] = item
+        self.storage = temp
 
 
 if __name__ == "__main__":
